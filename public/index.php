a204ca2c2d65e4f87bac5786906c15a0256fc339
mmcbbq <m.martinez@bbq.de>
2025-11-03 15:11:12 +0100

    kernel

--- public/index.php
+++ public/index.php
@@ -1,19 +1,23 @@
 <?php
 include '../config/loader.php';
-
+$kernel = new Kernel();
+$kernel->loadApp();
 
 $request =explode('/',$_SERVER['REQUEST_URI']) ;
 $entity = $request[1] ?? null;
 $method = $request[2] ?? null;
 $id = $request[3] ?? null;
-$controller =$entity.'Controller';
-if (file_exists("../src/Controller/".$controller.".php")){
-    $controller = new ($entity.'Controller');
+$controllerName = ucfirst($entity.'Controller')  ;
+if (file_exists("../src/Controller/".$controllerName.".php")){
+    $controller = new $controllerName;
+    $reflection = new ReflectionClass($controller);
+    if ($reflection->hasMethod($method)){
+        call_user_func_array([$controller, $method],[$id]);
+    }else{
+        http_response_code(404);
+        echo "Doe methode gibt es nicht";
 
-
-
-
-
+    }
 
 
 }else{
---
+++ src/Kernel.php
@@ -0,0 +1,26 @@
+<?php
+
+class Kernel
+{
+    
+    private string $controller;
+    private string $method;
+    private int $id;
+
+
+    public function loadController()
+    {
+        
+    }
+    public function loadMethod()
+    {
+        
+    }
+
+    public function loadApp()
+    {
+        
+    }
+
+    
+}
